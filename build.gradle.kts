260da68cb06eead7480ab8615a31a72180ba4c06
Pat L <pat.looft@gmail.com>
2022-07-19 08:41:06 -0400

    added the dependencies via gradle and we got it building after adding the properties file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	//base plugins from initializer
 	id("org.springframework.boot") version "2.7.1"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.21"
@@ -15,13 +16,29 @@ repositories {
 	mavenCentral()
 }
 
+val exposedVersion: String by project
 dependencies {
+	//base dependencies from the initializer
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	//dependencies from previous project:
+	//exposed imports
+	implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+	implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+	implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+	implementation("org.jetbrains.exposed:exposed-jodatime:$exposedVersion")
+	//or    implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+	//sql config
+	implementation("mysql:mysql-connector-java:8.0.27")
+	//hikari config
+	implementation( files("C:\\Users\\patlo\\Desktop\\Development\\jars\\HikariCP-5.0.0.jar"))
+	//log4j
+	implementation("org.slf4j:slf4j-nop:1.7.30")
 }
 
 tasks.withType<KotlinCompile> {
