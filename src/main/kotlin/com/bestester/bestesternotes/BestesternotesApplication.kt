59bb5cad62e9324a023dbf9ec0a04e51c31c7744
Pat L <pat.looft@gmail.com>
2022-07-19 08:29:15 -0400

    adding all files from the springboot initializer zip

---
+++ src/main/kotlin/com/bestester/bestesternotes/BestesternotesApplication.kt
@@ -0,0 +1,11 @@
+package com.bestester.bestesternotes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BestesternotesApplication
+
+fun main(args: Array<String>) {
+	runApplication<BestesternotesApplication>(*args)
+}
